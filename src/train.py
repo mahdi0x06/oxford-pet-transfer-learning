a0e80686b499423f54ac3f8e7eefe0cc9aa9b1d0
mahdi0x06 <mahdi.m3.8406@gmail.com>
2026-08-25 20:42:04 +0330

    Add training metrics visualization

--- src/train.py
+++ src/train.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from src.data import get_dataloaders
 from src.model import get_model
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
 def train():
     train_loader, val_loader = get_dataloaders()
@@ -22,6 +23,10 @@ def train():
 
     num_epochs = 1
     best_val_accuracy = 0.0
+    train_losses = []
+    train_accuracies = []
+    val_losses = []
+    val_accuracies = []
 
     for epoch in range(num_epochs):
 
@@ -75,6 +80,11 @@ def train():
                 model.state_dict(), 
                 "outputs/checkpoints/best_model.pth"
             )
+
+        train_losses.append(epoch_loss)
+        train_accuracies.append(train_accuracy)
+        val_losses.append(avg_val_loss)
+        val_accuracies.append(val_accuracy)
         print(
             f"Epoch {epoch + 1}/{num_epochs} - "
             f"Train Loss: {epoch_loss:.4f} - "
@@ -82,5 +92,41 @@ def train():
             f"Val Loss: {avg_val_loss:.4f} - "
             f"Val Acc: {val_accuracy:.2f}%"
         )
+
+    return train_losses, train_accuracies, val_losses, val_accuracies
+
+def plot_losses(train_losses, val_losses):
+    epochs = range(1, len(train_losses) + 1)
+
+    plt.figure()
+    plt.plot(epochs, train_losses, label="Train Loss")
+    plt.plot(epochs, val_losses, label="Validation Loss")
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Loss")
+    plt.title("Training and Validation Loss")
+    plt.legend()
+
+    plt.savefig("outputs/figures/loss_curve.png")
+    plt.close()
+
+def plot_accuracies(train_accuracies, val_accuracies):
+    epochs = range(1, len(train_accuracies) + 1)
+
+    plt.figure()
+    plt.plot(epochs, train_accuracies, label="Train Accuracy")
+    plt.plot(epochs, val_accuracies, label="Validation Accuracy")
+
+    plt.xlabel("Epoch")
+    plt.ylabel("Accuracy (%)")
+    plt.title("Training and Validation Accuracy")
+    plt.legend()
+
+    plt.savefig("outputs/figures/accuracy_curve.png")
+    plt.close()
+
 if __name__ == "__main__":
-    train()
+    train_losses, train_accuracies, val_losses, val_accuracies = train()
+
+    plot_losses(train_losses, val_losses)
+    plot_accuracies(train_accuracies, val_accuracies)
